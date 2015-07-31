38cba19b51c2b7cd85a4ec9f8241d044c02e62c6
bgrove1974 <bgroveben@gmail.com>
2015-07-31 01:58:59 -0700

    Add README and complete first exercise.

---
+++ README.md
@@ -0,0 +1,7 @@
+This repo is cloned from https://github.com/alexch/learn_ruby. Thanks Alex!
+
+I found this through The Odin Project http://www.theodinproject.com/
+
+and TestFirst.org http://testfirst.org/learn_ruby
+
+01_temperature
--- rspec_config.rb
+++ rspec_config.rb
@@ -2,3 +2,15 @@ RSpec.configure do |c|
   c.fail_fast = true
   c.color = true
 end
+
+#!# I added the following to suppress the should/expect syntax deprecation warnings I get when I run the rake command.
+
+RSpec.configure do |config|
+  # ...
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
